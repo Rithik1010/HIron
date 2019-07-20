1184e7bba6ee2d217166e02a1759c58d7b9a700e
Rithik1010 <rithik.roshan@codeastra.com>
2019-07-20 08:47:23 +0000

    Added seeded file

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :partner
+  validates :user_id, presence: true
+  validates :partner_id, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 User.create!(name:  "Ram",
-  email: "abcd@xyz.com",
+  email: "ram@xyz.com",
   password:              "foobar",
   password_confirmation: "foobar",
   activated: true,
   activated_at: Time.zone.now)
 
-99.times do |n|
+9.times do |n|
 name  = Faker::Name.name
 email = "example-#{n+1}@xyz.com"
 password = "password"
@@ -15,4 +15,15 @@ User.create!(name:  name,
    password_confirmation: password,
    activated: true,
    activated_at: Time.zone.now)
+end
+
+Service.create!(serv_name:  "Laundry")
+Service.create!(serv_name:  "Dish Wash")
+Service.create!(serv_name:  "Vehicle Wash")
+Service.create!(serv_name:  "Electric Repair")
+
+services = Service.order(:created_at).take(4)
+8.times do
+  name  = Faker::Name.name
+  services.each { |service| service.partners.create!(partner_name: name) }
 end
