bf1595823d1dc335450905395c75364b3459ce85
Rithik1010 <rithik.roshan@codeastra.com>
2019-07-21 11:33:04 +0000

    Added booking page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,14 +1,40 @@
 class BookingsController < ApplicationController
+  
+  def new
+    @booking = Booking.new
+  end
+  
   def create
-    @booking = booking.new(booking_params)
-    if @booking.save
-      flash[:success] = "Service Booked!"
-      redirect_to root_url
+    @booking = Booking.new(param[:id])
+    if @booking.save 
+      flash[:notice] = "Booked Succesfully"
+      #render :index
     else
-      render 'new'
+      flash[:notice] = 'Sorry! Booking was not done'
+      #render :new
     end
   end
   
+  def index
+    @user = current_user
+    @bookings = Booking.all
+  end
+  
   def show
+    @booking = Booking.find(params[:id])
+  end
+  
+  def update
+    @booking = Booking.find(params[:id])
+    if @booking.update_attributes(booking_params)
+        # if update is successful
+    else 
+        # if update is unsuccessful
+    end
+    redirect_to booking_path
+  end
+  
+  def booking_params
+    params.permit(:user_id, :partner_id)
   end
 end
--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -3,6 +3,14 @@ class PartnersController < ApplicationController
   end
   
   def show
+    get_booking
+  end
+  
+  private
+  
+  def get_booking
     @partner = Partner.find(params[:id])
+    @booking = Booking.create(user_id: current_user.id, partner_id: params[:id])
+    #redirect_to booking_url
   end
 end
