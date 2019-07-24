6ad95f5524df8a2d94c96e47549f5303ca4ac7bf
Rithik1010 <rithik.roshan@codeastra.com>
2019-07-24 06:02:14 +0000

    Added edit, delete to reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :find_partner
+  before_action :find_review, only: [:edit, :update, :destroy]
   
   def new
     @review = Review.new
@@ -17,6 +18,22 @@ class ReviewsController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @review.update(review_params)
+      redirect_to partner_path(@partner)
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @review.destroy
+    redirect_to partner_path(@partner)
+  end
+  
   private
     
     def review_params
@@ -27,5 +44,8 @@ class ReviewsController < ApplicationController
       @partner = Partner.find(params[:partner_id])
     end
       
+    def find_review
+      @review = Review.find(params[:id])
+    end
   
 end
