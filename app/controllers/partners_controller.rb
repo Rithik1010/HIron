3e3f6ffb09b18c187dc7023171473ac265283ce6
Rithik1010 <rithik.roshan@codeastra.com>
2019-07-24 07:48:04 +0000

    Added average rating to partners

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -4,5 +4,11 @@ class PartnersController < ApplicationController
   
   def show
     @partner = Partner.find(params[:id])
+    if @partner.reviews.blank?
+      @average_review = 0
+    else
+      @average_review = @partner.reviews.average(:rating).round(2)
+    end
   end
 end
+
